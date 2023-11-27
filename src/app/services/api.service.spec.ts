025c13502495cda6ed5210e18b692a1740a5acf5
vivekjha3072 <142294227+vivekjha3072@users.noreply.github.com>
2023-11-27 14:50:12 +0530

    updated api.service and added testcsae

--- src/app/services/api.service.spec.ts
+++ src/app/services/api.service.spec.ts
@@ -1,16 +1,38 @@
 import { TestBed } from '@angular/core/testing';
 
 import { ApiService } from './api.service';
+import { HttpClientModule } from '@angular/common/http';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 
 describe('ApiService', () => {
   let service: ApiService;
+  let httpTestingController: HttpTestingController;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule , HttpClientTestingModule],
+      providers: [ApiService]
+    });
     service = TestBed.inject(ApiService);
+    httpTestingController = TestBed.inject(HttpTestingController);
+  });
+
+  afterEach(() => {
+    httpTestingController.verify();
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+  it('should retrieve user data via GET', () => {
+    const mockUserData = { login: 'testUser', name: 'Test User' };
+
+    service.getUser('testUser').subscribe(data => {
+      expect(data).toEqual(mockUserData);
+    });
+
+    const req = httpTestingController.expectOne('https://api.github.com/users/testUser');
+    expect(req.request.method).toEqual('GET');
+    req.flush(mockUserData);
   });
 });
--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -1,19 +1,30 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient,HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { tap, throwError } from 'rxjs';
-
+import { Observable, throwError } from 'rxjs';
+import { catchError} from 'rxjs';
 @Injectable({
   providedIn: 'root'
 })
 export class ApiService {
 
-  constructor(
-    private httpClient: HttpClient
-  ) { }
+  private apiUrl = 'https://api.github.com/users';
 
-  getUser(githubUsername: string) {
-    return this.httpClient.get(`https://api.github.com/users/${githubUsername}`);
+  constructor(private httpClient: HttpClient) { }
+
+  getUser(githubUsername: string): Observable<any> {
+    return this.httpClient.get(`${this.apiUrl}/${githubUsername}`).pipe(catchError(this.handleError));
   }
 
   // implement getRepos method by referring to the documentation. Add proper types for the return type and params 
+  getRepos(githubUsername:string, page: number = 1, perPage: number = 10): Observable<any[]>{
+    const params = new HttpParams().set('page', page.toString()).set('per_page', perPage.toString());
+    return this.httpClient.get<any[]>(`${this.apiUrl}/${githubUsername}/repos`, { params }).pipe(catchError(this.handleError));
+  }
+  getLanguages(languagesUrl: string): Observable<any> {
+    return this.httpClient.get<any>(languagesUrl).pipe(catchError(this.handleError));
+  }
+  private handleError(error:any){
+    console.error('API Service Error:',error);
+    return throwError('An error occurred while calling the API');
+  }
 }
